a7af09ecb9f3ddbeb3178d00a370a7c9ef6b7c5e
salmanf60 <salman@pentagoat.com>
2025-05-22 15:56:21 +0530

    add environment endpoint

--- app.js
+++ app.js
@@ -15,6 +15,21 @@ app.get("/", (req, res) => {
     }
 });
 
+app.get("/environment", (req, res) => {
+    try {
+        console.log("add prod env");
+        res.json({
+            "status": 200,
+            "message": "add prod env Learning Jenkins..."
+        });
+    } catch (error) {
+        res.json({
+            "status": 500,
+            "error": error
+        });
+    }
+});
+
 app.listen(9000,()=>{
     console.log("listening on 9000 port")
 });
